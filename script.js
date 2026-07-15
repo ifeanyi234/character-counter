dbfd671805e9fe401d0680ddc68541e41e826a2f
sabzyhacks <ei711283@gmail.com>
2026-07-15 23:19:12 +0100

    toggle button accesibility

--- script.js
+++ script.js
@@ -4,15 +4,20 @@ const postBtn = document.querySelector("#postBtn");
 const postInput = document.querySelector("#postInput");
 
 postInput.addEventListener("input", function () {
-  let remaining = 160 - postInput.value.length;
+  const length = postInput.value.length;
+  const remaining = 160 - length;
   charCount.innerHTML = `${remaining} characters left`;
 
-  if (postInput.value.length >= 160) {
-    console.log(remaining, postInput.value.length);
-    postInput.maxLength = 160;
-  }
+  //setting limit
+  if (length >= 160) postInput.maxLength = 160;
+
   // visual color change when char is less than 20
   remaining <= 20
     ? (charCount.style.color = "red")
-    : (charCount.style.color = "inherit");
+    : (charCount.style.color = "#666");
+
+  // toggle button accesibility
+  length === 0 || remaining <= 0
+    ? (postBtn.disabled = true)
+    : (postBtn.disabled = false);
 });
