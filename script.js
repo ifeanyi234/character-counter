431d6778fd40f800e962574dcfda520af84caa6d
sabzyhacks <ei711283@gmail.com>
2026-07-15 22:59:55 +0100

    Visual color change on text when char is less than 20

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ const postInput = document.querySelector("#postInput");
 postInput.addEventListener("input", function () {
   let remaining = 160 - postInput.value.length;
   charCount.innerHTML = `${remaining} characters left`;
+
   if (postInput.value.length >= 160) {
     console.log(remaining, postInput.value.length);
     postInput.maxLength = 160;
   }
+  // visual color change when char is less than 20
+  remaining <= 20
+    ? (charCount.style.color = "red")
+    : (charCount.style.color = "inherit");
 });
