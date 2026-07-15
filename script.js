65b47f3fca6ba6a0786ffb68b7151c038aa29a11
sabzyhacks <ei711283@gmail.com>
2026-07-15 22:47:37 +0100

    Display remaining implemented

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-"Use script";
+"Use strict";
 const charCount = document.querySelector("#charCount");
 const postBtn = document.querySelector("#postBtn");
 const postInput = document.querySelector("#postInput");
 
 postInput.addEventListener("input", function () {
   let remaining = 160 - postInput.value.length;
+  charCount.innerHTML = `${remaining} characters left`;
   if (postInput.value.length >= 160) {
     console.log(remaining, postInput.value.length);
-    postInput.readOnly = true;
+    postInput.maxLength = 160;
   }
 });
